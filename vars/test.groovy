39d3fde214b6f8af6276caeaf779453fb8b441cf
Keyur Patel <patelkeyurj17@gmail.com>
2020-02-11 16:38:14 +0530

    Adding Groovy Scripts

---
+++ vars/test.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    stages {
+        stage("Stage 1"){
+            steps {
+              script {
+                  sh '''
+                    echo "Hello Stage 1"
+                  '''
+              }
+            }
+        } 
+        stage("Stage 2"){
+            steps {
+              script {
+                  sh '''
+                    echo "Hello Stage 2"
+                  '''
+              }   
+            }
+        }
+    }
+  }
