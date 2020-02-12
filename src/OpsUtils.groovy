cc060fbe72339ba94b039755d481f56bef14901a
Keyur Patel <patelkeyurj17@gmail.com>
2020-02-12 16:47:19 +0530

    Created a Pod Template for Jenkins Slave

---
+++ src/OpsUtils.groovy
@@ -0,0 +1,13 @@
+def getPodTemplate(){
+"""
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    jenkins_role: slave
+spec:
+  containers:
+    - name: jenkins_slave
+      image: keyurbitw/jenkinsdockerslave
+"""
+}
