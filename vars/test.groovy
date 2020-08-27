67430bf5b632c93038ee744389e657cdb0266e46
Keyur Patel <58063145+keyurbitw@users.noreply.github.com>
2020-08-27 20:36:12 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@ def call(body) {
   def opsUtils = new podtemplate.OpsUtils()
   pipeline {
     agent {
-      label 'master'
+      label 'docker-slave'
     }
     parameters {
       choice(name: 'CREATE_WEBEX_ROOM', choices: ['YES', 'NO'], description: 'Select this to skip webex space creation')
@@ -23,21 +23,6 @@ def call(body) {
             println env.WORKSPACE
           }   
         }
-      }
-  	  stage('Run Python Script'){
-        steps{
-          script {
-            try{
-              String request = libraryResource "script/Main.py"
-              def cmd = "/usr/bin/python request"
-              echo "Executing: ${cmd}"
-              sh cmd
-            }
-            catch(Exception e){
-              println e
-            }
-          }   
-	      }
       }
     }
   }
